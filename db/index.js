2492192e79713b37123e5a83a7b3735c9e659c33
Nicolas Ramirez <nicolas.ramirez.ka@icloud.com>
2020-04-05 13:55:15 +0200

    changing URI var name

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const db = mongoose.connection;
 
 const connectToMongoose = function () {
-  mongoose.connect(process.env.DB_URI, {
+  mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true 
   });
